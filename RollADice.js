2bdc97b8f3c6bdc4f5ac59003d6c9b355b97f9de
manvantar <manukvshetty@gmail.com>
2021-05-06 20:06:58 +0530

    Added:Logic to add two dices

--- RollADice.js
+++ RollADice.js
@@ -1,2 +1,3 @@
-let random=Math.floor((Math.random() * 6)+1);
-console.log(random);
+let random1=Math.floor((Math.random() * 6)+1);
+let random2=Math.floor((Math.random() * 6)+1);
+console.log("Summation of two dice: "+(random1+random2));
